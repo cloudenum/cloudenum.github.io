bc6dea3a6ba6de02a39281877e16a5b86b430404
dafapramudya <dafapramudya7@gmail.com>
2017-10-11 17:06:36 +0700

    m
    mn n

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
+
+$(document).ready(function(){
+  
+  //--------------//
+ // smoothscroll //
+//--------------//
+
+  $("a").on('click', function(event) {
+
+    // this.hash nggo jikot hash id
+    if (this.hash !== "") {
+
+      // ben ora balik default
+      event.preventDefault();
+
+      // simpen hash
+      var hash = this.hash;
+
+      // nganggo jQuery animate() men smooth le nyekrol
+      // 800 kue waktu animasi arep pirang ms
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // hash gari ditambahna maring URL nek animasine wis rampung dadi ora callback error
+        window.location.hash = hash;
+      });
+    } // END_IF
+  });
+/*end smoothscroll*/
+
+  //------------//
+ // scrollspy  //
+//------------//
+  $(window).scroll(function() {
+
+    var bodyOffset = $("#body").offset().top;
+    var eventTrigger = bodyOffset-120;
+
+    if ($(document).scrollTop() > eventTrigger) {
+      $('.navigasi').addClass('scrolled');
+      $('.topnav').children().css('color', 'black');
+
+      // $('.navigasi').animate({background: '#f7faff', filter: 'unset'}, 800);
+    } else {
+      $('.navigasi').removeClass("scrolled");
+      $('.topnav').children().css('color', '#f7faff');
+    }
+  });
+/*end scrollspy*/
+
+});
+
+
+
 var myTopNav = document.getElementById("myTopNav");
 var navigasi = document.getElementById("navigasi");
 
@@ -48,11 +103,20 @@ function showSlides(n) {
 // nggo layar sing cilik
 
 function sideNav() {
-    if (myTopNav.className === "topnav" && navigasi.className === "navigasi") {
+    if (myTopNav.className === "topnav" && navigasi.className === "navigasi") 
+    {
         myTopNav.className += ' responsive';
         navigasi.className += ' responsive';
-    } else {
+    }
+    else if (myTopNav.className === "topnav" && navigasi.className === "navigasi scrolled") 
+    {
+        myTopNav.className += ' responsive';
+        navigasi.className += ' responsive';
+    } 
+    else
+    {
         myTopNav.className = "topnav";
         navigasi.className = "navigasi";
     }
 }
+
