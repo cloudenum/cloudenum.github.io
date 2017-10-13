ccedeac94a20f7d54a62dbcc2c09b38a8abc635c
dafapramudya <dafapramudya7@gmail.com>
2017-10-13 19:53:51 +0700

    fix responsive, nav drawer
    fix responsive, nav drawer

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ function firstSlideContent(n) {
 
 }
 
+function openNav() {
+    document.getElementById("Sidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+}
+
 function lastSlideContent(n) {
   showSlideContent(jumlahSlide = n);
 }
