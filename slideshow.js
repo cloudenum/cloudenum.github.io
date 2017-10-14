cfde490b3b727f966e18388331fb1b69f915c9af
dafapramudya <dafapramudya7@gmail.com>
2017-10-14 10:40:20 +0700

    add gallery and fix responsive
    add gallery and fix responsive

--- slideshow.js
+++ slideshow.js
@@ -92,3 +92,40 @@ function showSlideContent(n) {
   slideContent[jumlahSlide-1].style.display = "block"; 
   pagenum[jumlahSlide-1].className += " active";
 }
+
+function openModalGal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModalGal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlidesGal(slideIndex);
+
+function plusSlidesGal(n) {
+  showSlidesGal(slideIndex += n);
+}
+
+function currentSlideGal(n) {
+  showSlidesGal(slideIndex = n);
+}
+
+function showSlidesGal(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
